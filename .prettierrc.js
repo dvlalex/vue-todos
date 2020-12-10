482ec859747c49fd27a76a98cc602d0d5ea2c346
Alex <dvlalex@gmail.com>
2020-12-10 17:54:02 +0200

    chore: cleanup & styling

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  tabWidth: 2,
+  printWidth: 120,
+}
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
-};
+  presets: ['@vue/cli-plugin-babel/preset'],
+  comments: process.env.NODE_ENV === 'development',
+}
